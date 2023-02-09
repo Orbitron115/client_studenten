9df0b8eb68c57dc671a2d7a6f4e68100843ff05b
Robin Wols <104897626+Orbitron115@users.noreply.github.com>
2023-02-09 12:33:16 +0100

    update

--- src/js/gdpr.js
+++ src/js/gdpr.js
@@ -2,10 +2,7 @@ class GDPR {
 
     constructor() {
         debugger;
-        this.showStatus();
-        this.showContent();
         this.bindEvents();
-
         if(this.cookieStatus() !== 'accept' && this.cookieStatus() !== 'reject') this.showGDPR();
     }
 
@@ -13,8 +10,6 @@ class GDPR {
         let buttonAccept = document.querySelector('.gdpr-consent__button--accept');
         buttonAccept.addEventListener('click', () => {
             this.cookieStatus('accept');
-            this.showStatus();
-            this.showContent();
             this.hideGDPR();
         });
 
@@ -23,8 +18,6 @@ class GDPR {
         let buttonReject = document.querySelector('.gdpr-consent__button--reject');
         buttonReject.addEventListener('click', () => {
             this.cookieStatus('reject');
-            this.showStatus();
-            this.showContent();
             this.hideGDPR();
         });
 
@@ -32,38 +25,10 @@ class GDPR {
     }
 
 
-    showContent() {
-        this.resetContent();
-        const status = this.cookieStatus() == null ? 'not-chosen' : this.cookieStatus();
-        const element = document.querySelector(`.content-gdpr-${status}`);
-        element.classList.add('show');
-
-    }
-
-    resetContent(){
-        const classes = [
-            '.content-gdpr-accept',
-
-//student uitwerking
-
-            '.content-gdpr-not-chosen'];
-
-        for(const c of classes){
-            document.querySelector(c).classList.add('hide');
-            document.querySelector(c).classList.remove('show');
-        }
-    }
-
-    showStatus() {
-        document.getElementById('content-gpdr-consent-status').innerHTML =
-            this.cookieStatus() == null ? 'Niet gekozen' : this.cookieStatus();
-    }
-
     cookieStatus(status) {
         if (status) localStorage.setItem('gdpr-consent-choice', status);
 
-//student uitwerking
-
+        
         return localStorage.getItem('gdpr-consent-choice');
     }
 
