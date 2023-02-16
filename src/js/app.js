0e7c56a446d144a96df5ad31d0eb3ab326c34ddd
Robin Wols <104897626+Orbitron115@users.noreply.github.com>
2023-02-16 14:12:21 +0100

    werkende captcha

--- src/js/app.js
+++ src/js/app.js
@@ -11,50 +11,30 @@ form.addEventListener("submit", async (event) => {
     // Then we prevent the form from being sent by canceling the event
     event.preventDefault();
 
+    if (document.querySelector('[name="h-captcha-response"]').value == ""){
+        alert("please complte the captcha")
+    }else{
     
-     if (validateInput() == false){
-        return;
-     }
-
-     /*
-    if (!email.validity.valid) {
-        // If it isn't, we display an appropriate error message
-        showError();
-        return;
-    }
-    */
-    
-    let response = await fetch('http://localhost:3000/form', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({email: email.value , subject: subject.value , description: description.value, name: name.value})
-    });
-
-    let data = await response.json();
-    sendMail()
-    alert(JSON.stringify(data))
+        if (validateInput() == false){
+           return;
+        }
+       
+       let response = await fetch('http://localhost:3000/form', {
+           method: 'POST',
+           headers: { 'Content-Type': 'application/json' },
+           body: JSON.stringify({email: email.value , subject: subject.value , description: description.value, name: name.value})
+       });
+   
+       let data = await response.json();
+       sendMail()
+       alert(JSON.stringify(data))
+   
+   }
 });
 
 function showErrorEmail() {
 
-    /*
-    if (email.validity.valueMissing) {
-        // If the field is empty,
-        // display the following error message.
-        emailError.textContent = "You need to enter an e-mail address.";
-    } else if (email.validity.typeMismatch) {
-        // If the field doesn't contain an email address,
-        // display the following error message.
-        emailError.textContent = "Entered value needs to be an e-mail address.";
-    } else if (email.validity.tooShort) {
-        // If the data is too short,
-        // display the following error message.
-        emailError.textContent = `E-mail should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
-    }
 
-    // Set the styling appropriately
-    emailError.className = "error active";
-    */
    alert("gaat wahet mis bij mail");
 }
 
@@ -120,4 +100,9 @@ function sendMail(){
     }).then(
       message => alert(message)
     );
+}
+
+function hCaptchaCallback(token){
+    form.setAttribute("data-hcaptcha-response" , token);
+
 }
